d768a399b141909b9eca06ce5d8e0fd1824d342e
“White-Ghost-Li” <872023908@qq.com>
2020-11-20 18:45:30 +0800

    first commit

---
+++ src/components/card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <text>card</text>
+    <p class="card">
+      {{text}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text']
+}
+</script>
+
+<style>
+.card {
+  padding: 10px;
+  color: red;
+}
+</style>
